cf21afd0adb8476667e87a5c387082534692e152
revaldo221126 <revaldoputra900@gmail.com>
2024-10-23 00:46:17 +0700

    Add files via upload

---
+++ latihanpesegipanjang.php
@@ -0,0 +1,16 @@
+<?php
+
+ function hitungluas($panjang, $lebar) {
+    $luas = $panjang * $lebar;
+    return $luas;
+ }
+
+
+
+ 
+ $panjang = 26;
+ $lebar = 25;
+ $luas = hitungLuas($panjang, $lebar);
+ echo "luas persegi panjang: " . $luas . " satuan persegi" . "</br>" 
+ 
+?>
